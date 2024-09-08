691826184b4308984be1a83bf6ed221c751b9df1
Hamza Mirza <hamza665@hotmail.de>
2024-09-08 09:17:43 +0200

    add material icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at libero cupiditate assumenda magnam minima ducimus amet rerum doloribus in nam placeat ipsum quaerat tempora accusantium, iusto voluptatum voluptas velit!</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at
+        libero cupiditate assumenda magnam minima ducimus amet rerum doloribus
+        in nam placeat ipsum quaerat tempora accusantium, iusto voluptatum
+        voluptas velit!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
