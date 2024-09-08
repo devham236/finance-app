86c0a994bddee7c5af6fa43988856459af20db8d
Hamza Mirza <hamza665@hotmail.de>
2024-09-08 09:09:45 +0200

    add styles folder and add google font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at libero cupiditate assumenda magnam minima ducimus amet rerum doloribus in nam placeat ipsum quaerat tempora accusantium, iusto voluptatum voluptas velit!</p>
     </div>
   )
 }
