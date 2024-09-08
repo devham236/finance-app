26478646e2f74ee79cb926aa0603d309cd61fe7f
Hamza Mirza <hamza665@hotmail.de>
2024-09-08 13:10:12 +0200

    clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias at
-        libero cupiditate assumenda magnam minima ducimus amet rerum doloribus
-        in nam placeat ipsum quaerat tempora accusantium, iusto voluptatum
-        voluptas velit!
-      </p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
