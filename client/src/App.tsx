08eafa55402079a01ebc968c234998ed64f812a9
Hamza Mirza <hamza665@hotmail.de>
2024-09-14 10:15:36 +0200

    added first colors and container styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="w-full h-full bg-container_color rounded-lg shadow-lg">
+      Lorem ipsum
+    </div>
+  );
 };
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        container_color: "#d4d4d8",
+      },
+    },
   },
   plugins: [],
 };
