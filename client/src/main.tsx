5b952421ce2048876478aa3e81242219158d419a
Hamza Mirza <hamza665@hotmail.de>
2024-09-14 10:29:09 +0200

    added redux state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { configureStore } from "@reduxjs/toolkit";
 
 import App from "./App.tsx";
 import "./styles/index.css";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {},
+});
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 );
