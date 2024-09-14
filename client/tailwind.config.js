06d4166f48fd3002e6b3bbe705dd2b88502430f7
Hamza Mirza <hamza665@hotmail.de>
2024-09-14 11:37:53 +0200

    updated custom colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,10 @@ export default {
   theme: {
     extend: {
       colors: {
+        body_color_light: "#d4d4d8",
+        body_color_dark: "#334155",
         container_color_light: "#e4e4e7",
-        container_color_dark: "#64748b",
+        container_color_dark: "#475569",
         text_color_light: "#4d4d4d",
         text_color_dark: "#f0f0f0",
       },
