5ffb729565bc4024a2a375270ac2883dfbb8c75c
Hamza Mirza <hamza665@hotmail.de>
2024-09-15 10:47:52 +0200

    added icons

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="h-full text-center border-r-2 border-slate-50 flex flex-col p-8 text-text_color_light">
+    <div className="h-full text-center border-r-2 border-slate-50 flex flex-col p-[1.5rem] text-text_color_light">
       <Link
         to={"/overview"}
-        className="hover:bg-[#f9fafb] duration-200 p-2 hover:rounded-md hover:shadow-md mb-6 cursor-pointer"
+        className="hover:bg-[#f9fafb] duration-200 p-2 hover:rounded-md hover:shadow-md mb-6 cursor-pointer flex items-center"
       >
-        <span>Overview</span>
+        <span className="material-symbols-rounded mr-2">grid_view</span>
+        <p>Overview</p>
       </Link>
       <Link
         to={"/settings"}
-        className="hover:bg-[#f9fafb] duration-200 p-2 hover:rounded-md hover:shadow-md mb-6 cursor-pointer"
+        className="hover:bg-[#f9fafb] duration-200 p-2 hover:rounded-md hover:shadow-md mb-6 cursor-pointer flex items-center"
       >
-        <span>Settings</span>
+        <span className="material-symbols-rounded mr-2">settings</span>
+        <p>Settings</p>
       </Link>
     </div>
   );
--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -5,8 +5,8 @@ const Topbar = () => {
     <div className="w-full p-8 border-b-2 border-slate-50 flex items-center justify-between text-text_color_light">
       <div>Logo</div>
       <div>
-        <span className="mr-24">Search</span>
-        <span className="mr-24">Settings</span>
+        <span className="mr-20">Search</span>
+        <span className="mr-20">DarkMode</span>
         <span className="">Account</span>
       </div>
     </div>
