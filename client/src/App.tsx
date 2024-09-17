642ca097e03eba9ce961e3c0bf7c61f3ac1462ee
Hamza Mirza <hamza665@hotmail.de>
2024-09-17 11:47:07 +0200

    restructure elements to change body color

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,19 +8,17 @@ const App = () => {
   const { isDarkMode } = useSelector((state: any) => state.darkmode);
 
   return (
-    <div
-      className={`w-full h-full bg-body_color_light flex items-center justify-center ${
-        isDarkMode && "dark"
-      }`}
-    >
-      <div className="w-full h-full max-w-[1500px] max-h-[900px] bg-container_color_light dark:bg-container_color_dark rounded-xl shadow-lg">
-        <Topbar />
-        <div className="w-full h-[calc(100%-90px)] flex">
-          <Sidebar />
-          <MainContent />
+    <main className={`w-[100vw] h-[100vh] ${isDarkMode && "dark"}`}>
+      <div className="w-full h-full bg-body_color_light dark:bg-body_color_dark flex items-center justify-center">
+        <div className="w-full h-full max-w-[1500px] max-h-[900px] bg-container_color_light dark:bg-container_color_dark rounded-xl shadow-lg">
+          <Topbar />
+          <div className="w-full h-[calc(100%-98px)] flex">
+            <Sidebar />
+            <MainContent />
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
