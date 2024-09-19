480ab25f817696d72133d126f6106d91ee9c33d8
Hamza Mirza <hamza665@hotmail.de>
2024-09-19 14:47:27 +0200

    simple server setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log("test");
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+server.listen(8000, () => {
+  console.log("Server is listening on Port: 8000");
+});
