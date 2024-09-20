28f9e8cea420c8f350d71a44b9e227ddaa28a1bf
Hamza Mirza <hamza665@hotmail.de>
2024-09-20 15:20:07 +0200

    search box in topbar

--- client/src/components/Topbar.tsx
+++ client/src/components/Topbar.tsx
@@ -12,7 +12,14 @@ const Topbar = () => {
     <div className="w-full p-8 border-b-2 border-slate-50 dark:border-opacity-20 flex items-center justify-between text-text_color_light dark:text-text_color_dark">
       <div className="text-green_color">Ruune</div>
       <div className="flex items-center">
-        <span className="mr-20 cursor-pointer">Search</span>
+        <div className="mr-20 flex items-center p-1 rounded-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark">
+          <span className="material-symbols-rounded mr-2">search</span>
+          <input
+            type="text"
+            placeholder="Search"
+            className=" bg-transparent text-text_color_light placeholder:text-text_color_light dark:text-text_color_dark dark:placeholder:text-text_color_dark outline-none"
+          />
+        </div>
         <div
           className="mr-20 cursor-pointer bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark flex items-center justify-between px-1 py-1 rounded-[20px] relative"
           onClick={() => dispatch(toggleDarkMode())}
