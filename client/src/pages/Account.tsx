9cbbd01e2b7746afec65045332ebbdc64bfb5fa8
Hamza Mirza <hamza665@hotmail.de>
2024-09-20 11:28:44 +0200

    test if endpoint works

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const Account = () => {
+  useEffect(() => {
+    const testEndpoint = async () => {
+      const res = await axios.get("http://localhost:8000/api/v1/test");
+      console.log(res);
+    };
+
+    testEndpoint();
+  }, []);
   return (
     <div className="text-text_color_light dark:text-text_color_dark">
       Account
