3e1a25643736f0de9e6b90292cb52d5dd9bab5a1
Hamza Mirza <hamza665@hotmail.de>
2024-09-20 11:11:51 +0200

    simple endpoint setup

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div className="text-text_color_light dark:text-text_color_dark">
+      Account
+    </div>
+  );
 };
 
 export default Account;
--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "test" });
+});
+
 server.listen(8000, () => {
   console.log("Server is listening on Port: 8000");
 });
