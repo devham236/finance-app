3269860a3484b16cf6d814326860151acdc4c5e0
Hamza Mirza <hamza665@hotmail.de>
2024-09-21 11:04:34 +0200

    skeleton structure of signup form

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const SignupForm = () => {
-  return <div className="">Signup Form</div>;
+  return (
+    <div className="w-[500px] bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-md p-[1.5rem]">
+      <h2 className="text-xl font-bold">Sign Up</h2>
+      <p>
+        Already have an Account? <span>Login here.</span>
+      </p>
+      <div className="flex flex-col">
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </div>
+      <button>Sign Up</button>
+      <div className="flex items-center">
+        <p>Sign up with: </p>
+        <div className="w-[30px] h-[30px]">
+          <img
+            src="/assets/Google__G__logo.svg.png"
+            alt=""
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupForm;
