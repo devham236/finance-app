65df890afe5a476546402b6360bb1876a160a488
Hamza Mirza <hamza665@hotmail.de>
2024-09-21 10:08:54 +0200

    auth page added

--- client/src/pages/Account.tsx
+++ client/src/pages/Account.tsx
@@ -1,15 +1,6 @@
-import axios from "../axiosConfig";
-import React, { useEffect } from "react";
+import React from "react";
 
 const Account = () => {
-  useEffect(() => {
-    const testEndpoint = async () => {
-      const res = await axios.get("/test");
-      console.log(res);
-    };
-
-    testEndpoint();
-  }, []);
   return (
     <div className="text-text_color_light dark:text-text_color_dark">
       Account
