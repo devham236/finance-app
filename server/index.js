f79b6f191dc9bb20e6d3bc0d0ac4019512fec2d0
Hamza Mirza <hamza665@hotmail.de>
2024-09-29 12:49:33 +0200

    clean up backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import http from "http";
-import { auth, googleProvider } from "./firebase/firebaseConfig.js";
-import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,16 +8,7 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/v1/test", async (req, res) => {
-  const { email, password } = req.body;
-  const response = await createUserWithEmailAndPassword(auth, email, password);
-  console.log(response);
-
-  res.json({
-    message: `You created an account with following data: email ${email}, password ${password}`,
-    body: response,
-  });
-});
+app.post("/api/v1/test", async (req, res) => {});
 
 server.listen(8000, () => {
   console.log("Server is listening on Port: 8000");
