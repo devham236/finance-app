9a581ccdf79a1c5ac7f7de2823d77b773b7f382d
Hamza Mirza <hamza665@hotmail.de>
2024-10-01 08:59:12 +0200

    delete components

--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Overview from "../pages/Overview";
 import Settings from "../pages/Settings";
-import Account from "../pages/Account";
 
 const MainContent = () => {
   return (
@@ -11,7 +10,6 @@ const MainContent = () => {
         <Route path="/" element={<Overview />} />
         <Route path="/overview" element={<Overview />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/account" element={<Account />} />
       </Routes>
     </div>
   );
