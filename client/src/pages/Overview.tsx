f2b4c3c75517794fbe1d5312ae3084ca51c492b2
Hamza Mirza <hamza665@hotmail.de>
2024-10-03 12:46:48 +0200

    created custom class for grid items

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -3,21 +3,13 @@ import React from "react";
 const Overview = () => {
   return (
     <div className="text-text_color_light dark:text-text_color_dark w-full h-full  grid grid-rows-2 grid-cols-3 gap-[1.5rem]">
-      <div className="w-full h-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-xl">
-        Total Income
-      </div>
-      <div className="w-full h-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-xl">
-        Total Spent
-      </div>
-      <div className="w-full h-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-xl">
-        Unknown
-      </div>
-      <div className="w-full h-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-xl col-span-2">
+      <div className="grid-item">Total Income</div>
+      <div className="grid-item">Total Spent</div>
+      <div className="grid-item">Unknown</div>
+      <div className="grid-item col-span-2">
         Compare Income and Spent Graphs
       </div>
-      <div className="w-full h-full bg-sidebar_item_color_light dark:bg-sidebar_item_color_dark rounded-xl">
-        Goals
-      </div>
+      <div className="grid-item">Goals</div>
     </div>
   );
 };
