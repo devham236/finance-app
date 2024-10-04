6e2a9bbd8195c9e5a6f8c93502124fe1b27a1cbc
Hamza Mirza <hamza665@hotmail.de>
2024-10-04 11:06:11 +0200

    restructure component

--- client/src/components/DoughnutChart.tsx
+++ client/src/components/DoughnutChart.tsx
@@ -1,8 +1,8 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip);
 
 const DoughnutChart = () => {
   const data = {
@@ -19,7 +19,28 @@ const DoughnutChart = () => {
 
   const options = {};
 
-  return <Doughnut data={data} options={options}></Doughnut>;
+  return (
+    <>
+      <h3 className="font-[600] self-start">Total Income</h3>
+      <div className="w-56 h-56">
+        <Doughnut data={data} options={options}></Doughnut>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="flex items-center mr-4">
+          <span>dot</span>
+          <p> category</p>
+        </div>
+        <div className="flex items-center mr-4">
+          <span>dot</span>
+          <p> category</p>
+        </div>
+        <div className="flex items-center">
+          <span>dot</span>
+          <p> category</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DoughnutChart;
--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -5,10 +5,7 @@ const Overview = () => {
   return (
     <div className="text-text_color_light dark:text-text_color_dark w-full h-full  grid grid-rows-2 grid-cols-3 gap-[1.5rem]">
       <div className="grid-item p-[1.5rem] flex flex-col items-center justify-between">
-        <h3 className="font-[600] self-start">Total Income</h3>
-        <div className="w-56 h-56">
-          <DoughnutChart />
-        </div>
+        <DoughnutChart />
       </div>
       <div className="grid-item">Total Spent</div>
       <div className="grid-item">Unknown</div>
