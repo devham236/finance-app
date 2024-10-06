51da50e4b35f7adff449fe6a5857a5855d3f2a25
Hamza Mirza <hamza665@hotmail.de>
2024-10-06 13:42:42 +0200

    cleaned up main file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,79 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store.ts";
 
 import App from "./App.tsx";
 import "./styles/index.css";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-
-import sidebarReducer from "./slices/sidebarSlice.ts";
-import darkmodeReducer from "./slices/darkmodeSlice.ts";
-import userReducer from "./slices/userSlice.ts";
-import authFormReducer from "./slices/authFormSlice.ts";
-import doughnutChartReducer from "./slices/doughnutChartSlice.ts";
-import totalIncomeReducer from "./slices/totalIncomeSlice.ts";
-
-import storage from "redux-persist/lib/storage";
-import { PERSIST, persistReducer, persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-
-const persistDarkmodeConfig = {
-  key: "darkmode",
-  storage,
-};
-
-const persistSidebarConfig = {
-  key: "sidebar",
-  storage,
-};
-
-const persistUserConfig = {
-  key: "user",
-  storage,
-};
-
-const persistDoughnutConfig = {
-  key: "doughnutChart",
-  storage,
-};
-
-const persistedDarkmodeReducer = persistReducer(
-  persistDarkmodeConfig,
-  darkmodeReducer
-);
-
-const persistedSidebarReducer = persistReducer(
-  persistSidebarConfig,
-  sidebarReducer
-);
-
-const persistedUserReducer = persistReducer(persistUserConfig, userReducer);
-
-const persistedDoughnutReducer = persistReducer(
-  persistDoughnutConfig,
-  doughnutChartReducer
-);
-
-const store = configureStore({
-  reducer: {
-    sidebar: persistedSidebarReducer,
-    darkmode: persistedDarkmodeReducer,
-    user: persistedUserReducer,
-    authForm: authFormReducer,
-    doughnutChart: persistedDoughnutReducer,
-    totalIncome: totalIncomeReducer,
-  },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [PERSIST],
-      },
-    });
-  },
-});
-
-const persistor = persistStore(store);
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
---
+++ client/src/store/store.ts
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { PERSIST, persistStore } from "redux-persist";
+import {
+  persistedDarkmodeReducer,
+  persistedDoughnutReducer,
+  persistedSidebarReducer,
+  persistedUserReducer,
+} from "./persistReducers";
+import authFormReducer from "../slices/authFormSlice";
+import totalIncomeReducer from "../slices/totalIncomeSlice";
+
+export const store = configureStore({
+  reducer: {
+    sidebar: persistedSidebarReducer,
+    darkmode: persistedDarkmodeReducer,
+    user: persistedUserReducer,
+    authForm: authFormReducer,
+    doughnutChart: persistedDoughnutReducer,
+    totalIncome: totalIncomeReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [PERSIST],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
