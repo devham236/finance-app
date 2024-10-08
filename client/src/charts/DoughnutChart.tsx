57a9541424407ef7a96ec3d25f8931668ea6349a
Hamza Mirza <hamza665@hotmail.de>
2024-10-08 13:49:13 +0200

    check f user is logged in before adding new income

--- client/src/charts/DoughnutChart.tsx
+++ client/src/charts/DoughnutChart.tsx
@@ -5,19 +5,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { resetTotalIncome, setTotalIncome } from "../slices/totalIncomeSlice";
 import { toggleIncomeForm } from "../slices/incomeFormSlice";
 import { resetChartData } from "../slices/doughnutChartSlice";
+import { toggleForm } from "../slices/authFormSlice";
 
 ChartJS.register(ArcElement, Tooltip);
 
 const DoughnutChart = () => {
   const { chartData } = useSelector((state: any) => state.doughnutChart);
   const { incomeValue } = useSelector((state: any) => state.totalIncome);
-  const dispatch = useDispatch();
-
+  const { userData } = useSelector((state: any) => state.user);
   const config = {};
+  const dispatch = useDispatch();
 
   const resetData = () => {
     dispatch(resetChartData());
     dispatch(resetTotalIncome());
+  };
+
+  const addData = () => {
+    if (!userData || !userData.id || userData.id === "") {
+      dispatch(toggleForm(true));
+    } else {
+      dispatch(toggleIncomeForm());
+    }
   };
 
   useEffect(() => {
@@ -35,11 +44,13 @@ const DoughnutChart = () => {
     }
   }, [chartData.datasets, dispatch]);
 
+  console.log(userData);
+
   return (
     <div className="grid-item flex flex-col items-center justify-between relative">
       <div className="flex items-center justify-between w-full">
         <h3 className="font-[600] self-start">Total Income</h3>
-        <button onClick={() => dispatch(toggleIncomeForm())}>
+        <button onClick={addData}>
           <span className="material-symbols-rounded text-green_color">
             add_circle
           </span>
