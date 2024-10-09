ed6b0560f93540f22ea279785864685f615f3948
Hamza Mirza <hamza665@hotmail.de>
2024-10-09 06:57:50 +0200

    renamed grid item

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -6,7 +6,7 @@ const Overview = () => {
     <div className="text-text_color_light dark:text-text_color_dark w-full h-full  grid grid-rows-2 grid-cols-3 gap-[1.5rem]">
       <DoughnutChart />
       <div className="grid-item">Total Spent</div>
-      <div className="grid-item">Unknown</div>
+      <div className="grid-item">Trends</div>
       <div className="grid-item col-span-2">
         Compare Income and Spent Graphs
       </div>
