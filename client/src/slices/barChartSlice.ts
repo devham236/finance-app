331aceaf7fa427374fce43e494edbbabf4e8b265
Hamza Mirza <hamza665@hotmail.de>
2024-10-12 13:03:00 +0200

    added dummy data and reducers to barchart slice

--- client/src/slices/barChartSlice.ts
+++ client/src/slices/barChartSlice.ts
@@ -1,11 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { NewExpense } from "../utils/types/types";
 
 const barChartSlice = createSlice({
   name: "barChart",
   initialState: {
-    data: [],
+    data: [
+      {
+        id: 1,
+        expense: 200,
+        label: "Rent",
+        color: "red",
+      },
+      {
+        id: 2,
+        expense: 100,
+        label: "Groceries",
+        color: "yellow",
+      },
+    ],
   },
-  reducers: {},
+  reducers: {
+    addExpense: (state, action) => {},
+    resetBarChartData: (state, action) => {},
+  },
 });
 
+export const { addExpense, resetBarChartData } = barChartSlice.actions;
 export default barChartSlice.reducer;
