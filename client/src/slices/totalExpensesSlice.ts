6e1b4a648d4005b97c88b8580ea85f5def70d67c
Hamza Mirza <hamza665@hotmail.de>
2024-10-12 12:25:16 +0200

    added initialstate and reducers to toalexpeneses slice

--- client/src/slices/totalExpensesSlice.ts
+++ client/src/slices/totalExpensesSlice.ts
@@ -2,8 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const totalExpensesSlice = createSlice({
   name: "totalExpenses",
-  initialState: {},
-  reducers: {},
+  initialState: { expensesValue: 0 },
+  reducers: {
+    setTotalExpenses: (state, action) => {
+      state.expensesValue = action.payload;
+    },
+    resetTotalExpenses: (state) => {
+      state.expensesValue = 0;
+    },
+  },
 });
 
+export const { setTotalExpenses, resetTotalExpenses } =
+  totalExpensesSlice.actions;
 export default totalExpensesSlice.reducer;
