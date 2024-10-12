0b165d419130ec131021790d560ef1bd33224490
Hamza Mirza <hamza665@hotmail.de>
2024-10-12 10:30:56 +0200

    place reset button next to add button

--- client/src/charts/DoughnutChart.tsx
+++ client/src/charts/DoughnutChart.tsx
@@ -48,11 +48,20 @@ const DoughnutChart = () => {
     <div className="grid-item flex flex-col items-center justify-between relative">
       <div className="flex items-center justify-between w-full">
         <h3 className="font-[600] self-start">Total Income</h3>
-        <button onClick={addData}>
-          <span className="material-symbols-rounded text-green_color">
-            add_circle
-          </span>
-        </button>
+        <div className="flex items-center">
+          {incomeValue > 0 && (
+            <button className="mr-2" onClick={resetData}>
+              <span className="material-symbols-rounded opacity-50 hover:opacity-100 duration-200">
+                autorenew
+              </span>
+            </button>
+          )}
+          <button onClick={addData}>
+            <span className="material-symbols-rounded text-green_color">
+              add_circle
+            </span>
+          </button>
+        </div>
       </div>
       <div className="w-52 h-52">
         <Doughnut data={chartData} options={config}></Doughnut>
@@ -73,14 +82,6 @@ const DoughnutChart = () => {
       <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-xl font-bold">
         {incomeValue}€
       </p>
-      {incomeValue > 0 && (
-        <p
-          className="absolute cursor-pointer bottom-6 right-6 text-green_color hover:underline"
-          onClick={resetData}
-        >
-          Reset
-        </p>
-      )}
     </div>
   );
 };
