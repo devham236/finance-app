f015cb7744e1596ba0d42e15b18b654a240254c1
Hamza Mirza <hamza665@hotmail.de>
2024-10-12 12:32:49 +0200

    added barChart slice

---
+++ client/src/slices/barChartSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const barChartSlice = createSlice({
+  name: "barChart",
+  initialState: {
+    data: [],
+  },
+  reducers: {},
+});
+
+export default barChartSlice.reducer;
