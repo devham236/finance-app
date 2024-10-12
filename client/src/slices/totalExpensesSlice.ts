ecfa0bf5e4c576ab73c4a24c321b2fea40385d83
Hamza Mirza <hamza665@hotmail.de>
2024-10-12 12:20:30 +0200

    created new slice for totalExpenses

---
+++ client/src/slices/totalExpensesSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const totalExpensesSlice = createSlice({
+  name: "totalExpenses",
+  initialState: {},
+  reducers: {},
+});
+
+export default totalExpensesSlice.reducer;
