da5cfc97b467d88f040dbd64f9edafb0be1c4098
Hamza Mirza <hamza665@hotmail.de>
2024-10-23 09:21:15 +0200

    created new slice for line chart

---
+++ client/src/slices/lineChartSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const lineChartSlice = createSlice({
+  name: "lineChart",
+  initialState: {},
+  reducers: {},
+});
+
+export default lineChartSlice.reducer;
