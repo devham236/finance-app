b4cac6016f82611cee27c34bafd3c2ef780218f3
Hamza Mirza <hamza665@hotmail.de>
2024-10-23 10:22:14 +0200

    import different compoents

--- client/src/charts/LineChart.tsx
+++ client/src/charts/LineChart.tsx
@@ -1,8 +1,25 @@
-import { Chart as ChartJS, LineElement, Tooltip } from "chart.js";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-ChartJS.register(LineElement, Tooltip);
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const LineChart = () => {
   return <div className="grid-item col-span-2"></div>;
