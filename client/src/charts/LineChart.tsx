8fb18412abd9369257d1e2757acaa9f4507bf237
Hamza Mirza <hamza665@hotmail.de>
2024-10-23 09:36:12 +0200

    import line component

--- client/src/charts/LineChart.tsx
+++ client/src/charts/LineChart.tsx
@@ -1,7 +1,11 @@
+import { Chart as ChartJS, LineElement, Tooltip } from "chart.js";
 import React from "react";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(LineElement, Tooltip);
 
 const LineChart = () => {
-  return <div className="grid-item col-span-2">LineChart</div>;
+  return <div className="grid-item col-span-2"></div>;
 };
 
 export default LineChart;
