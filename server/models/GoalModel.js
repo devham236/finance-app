e624afa9e8f95d6b05d8688628bee542bd2c3011
Hamza Mirza <hamza665@hotmail.de>
2024-11-24 19:06:44 +0100

    added static db functions for goalModel

--- server/models/GoalModel.js
+++ server/models/GoalModel.js
@@ -9,11 +9,46 @@ const goalSchema = new mongoose.Schema({
   description: {
     type: Number,
   },
-  id: {
+  userId: {
     type: String,
     required: true,
     unique: true,
   },
+  achieved: {
+    type: Boolean,
+    required: true,
+    unique: false,
+  },
 });
+
+goalSchema.statics.addGoal = async function (goalObject) {
+  const { id, title, description, achieved } = goalObject;
+  if ((!title, !id)) {
+    throw new Error("Missing required field: title or id.");
+  }
+  try {
+    const newGoal = await this.create({
+      userId: id,
+      title,
+      description,
+      achieved,
+    });
+    return newGoal;
+  } catch (error) {
+    console.error("Database error:", error);
+    throw new Error("Failed to create goal in Database");
+  }
+};
+
+goalSchema.statics.getGoals = async function () {
+  try {
+    const goalsList = await this.find();
+    if (goalsList.length > 0) {
+      return goalsList;
+    }
+  } catch (error) {
+    throw new Error("Unable to fetch goals.");
+  }
+};
 
 export const GoalModel = mongoose.model("goals", goalSchema);
