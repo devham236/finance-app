3835ae00a3f4f67b000155058ba13531d002d8a6
Hamza Mirza <hamza665@hotmail.de>
2024-12-12 13:12:49 +0100

    clean up

--- client/src/components/Trends.tsx
+++ client/src/components/Trends.tsx
@@ -4,7 +4,7 @@ const Trends = () => {
   return (
     <div className="grid-item flex flex-col items-center justify-between">
       <h3 className="font-[600] self-start">Trends</h3>
-      <div className="border-4 border-container_color_light dark:border-container_color_dark flex flex-col items-center px-5 py-9 rounded-md">
+      <div className="border-2 border-container_color_light dark:border-container_color_dark flex flex-col items-center px-5 py-9 rounded-md">
         <div className="flex items-center">
           <p className="text-2xl font-bold mr-2">25%</p>
           <div className="bg-green_color flex items-center justify-center w-7 h-7 rounded-full">
