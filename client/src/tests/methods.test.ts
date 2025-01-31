9cc4db082b4a2ca82eddb9182c5da61a4639f7f3
Hamza Mirza <hamza665@hotmail.de>
2025-01-31 14:15:06 +0100

    deleted test folder

--- client/src/tests/methods.test.ts
+++
@@ -1,9 +0,0 @@
-import { calcExpensePercentage } from "../utils/helpers/methods";
-
-test("should calculate percentage correctly", () => {
-  expect(calcExpensePercentage(500, 1000)).toBe("50.00");
-});
-
-test("should return a string", () => {
-  expect(typeof calcExpensePercentage(500, 1000)).toBe("string");
-});
