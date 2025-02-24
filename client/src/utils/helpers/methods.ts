752acefa299db15516435898cd41bc11ae48c0e3
Hamza Mirza <hamza665@hotmail.de>
2025-02-24 10:15:39 +0100

    clean up methods

--- client/src/utils/helpers/methods.ts
+++ client/src/utils/helpers/methods.ts
@@ -22,25 +22,3 @@ export const getYearsFromTimeAccCreated = (timestamp: string): number[] => {
   }
   return years;
 };
-
-export const getMonthsFromTimeAccCreated = (timestamp: string): number[] => {
-  const startMonth = getMonthFromTimeStamp(timestamp);
-  const currentMonth = new Date().getMonth();
-  let months = [];
-  for (let i = 0; i <= 9; i++) {
-    months.push(i);
-  }
-  return months;
-
-  // months are numbers from 0 to 11 => 0 is January, 1 is February, 2 is March, etc.
-  // startMonth is the month the account was created by the user
-  // currentMonth is the current month
-  // we want to return an array of months from startMonth to currentMonth
-
-  // if the account was created in January and it's currently January, we should return [0]
-  // if the account was created in January and it's currently February, we should return [0, 1]
-  // if the account was created in December and it's currently January, we should return [11, 0]
-
-  // if after the number 11, so December, we have a 0, so January, it means the timespan goes from one year to the next
-  // that means the year count should go up by one
-};
