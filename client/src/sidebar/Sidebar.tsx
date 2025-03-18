3f278830af735a19f003193992644b0dd9b7bf93
Hamza Mirza <hamza665@hotmail.de>
2025-03-18 11:50:26 +0100

    clean up

--- client/src/sidebar/Sidebar.tsx
+++ client/src/sidebar/Sidebar.tsx
@@ -4,8 +4,6 @@ import { useSelector } from "react-redux";
 const Sidebar = ({ children }: { children: ReactNode }) => {
   const { currentPath } = useSelector((state: any) => state.sidebar);
 
-  console.log(currentPath);
-
   return (
     <section className="h-full text-center border-r-2 border-slate-50 dark:border-opacity-20 flex flex-col p-[1.5rem] text-text_color_light dark:text-text_color_dark relative">
       {children}
