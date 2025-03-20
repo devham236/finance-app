2d7b2a2d1de098f3d889c4191729f2cb83bfcf86
Hamza Mirza <hamza665@hotmail.de>
2025-03-20 09:22:28 +0100

    added method to utils folder

--- client/src/utils/helpers/methods.ts
+++ client/src/utils/helpers/methods.ts
@@ -4,3 +4,16 @@ export const calcExpensePercentage = (
 ) => {
   return ((expense / totalExpenses) * 100).toFixed(2);
 };
+
+export const getInitialState = (entryType: string) => {
+  switch (entryType) {
+    case "Income":
+      return { income: 0, label: "", color: "#3e9c35" };
+    case "Expense":
+      return { expense: 0, label: "", color: "#3e9c35" };
+    case "Goal":
+      return { title: "", description: "", achieved: false };
+    default:
+      return {};
+  }
+};
